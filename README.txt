5e55c44c7fdb8ee0f85aff467f683130faf91c8e
BrycePaquette <bryce.paquette@canarysystems.com>
2022-01-24 20:50:45 -0500

    Completed styling of Signup page

--- README.txt
+++ README.txt
@@ -16,15 +16,11 @@ Step 1 - Creating a database of exercises to choose from in the app
 
 Step 2 - Initializing a Flask App from Scratch
 * Flask QuickStart: https://flask.palletsprojects.com/en/2.0.x/quickstart/
-* Turned on debug mode: $env:FLASK_ENV = "development" https://flask.palletsprojects.com/en/2.0.x/quickstart/#debug-mode
+* Turned on debug mode: <$env:FLASK_ENV = "development"> https://flask.palletsprojects.com/en/2.0.x/quickstart/#debug-mode
+* Debug app.py in vscode debugger: https://code.visualstudio.com/docs/python/tutorial-flask
 * Template Inheritance: https://flask.palletsprojects.com/en/2.0.x/patterns/templateinheritance/
 * Created layout.html as base template to extend from. Created index.html
 * Manage dependencies by using pipreqs: https://pypi.org/project/pipreqs/
-* Using flask login for authentication: https://flask-login.readthedocs.io/en/latest/ 
-                                        https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
-* Using flask-sqlalchemy to communicate with sqlite3 db: https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
-* Flask Sessions feature: https://flask.palletsprojects.com/en/2.0.x/quickstart/#sessions
-* Run this cmd to generate a secret key for sessions: <python -c 'import secrets; print(secrets.token_hex())'>
 
 Step 3 - Connecting to git and pulling/pushing
 * CS50 Web Class on Git: https://cs50.harvard.edu/web/2020/notes/1/
@@ -34,3 +30,10 @@ Step 4 - Starting the Web App UI Design
 * Free Icon Site: https://phosphoricons.com/ (No Python Flask support, need to use svg or png)
 * Free App UI Designer Tool: https://framer.com/
 * SVG formats can be copied into an html page for rendering. <img> tags don't work for this format
+
+Step 5 - Adding Authentication
+* Using flask login for authentication: https://flask-login.readthedocs.io/en/latest/ 
+                                        https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
+* Using flask-sqlalchemy to communicate with sqlite3 db: https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+* Flask Sessions feature: https://flask.palletsprojects.com/en/2.0.x/quickstart/#sessions
+* Run this cmd to generate a secret key for sessions: <python -c 'import secrets; print(secrets.token_hex())'>
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, sessions
+from flask import Flask, render_template, request, sessions, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 """
 Possible Future Enhancements:
 * utilize the Blueprint class in flask to organize routes into multiple files. Examples: https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
+* implement flashing messages
 """
 
 app = Flask(__name__)
@@ -13,22 +15,84 @@ app = Flask(__name__)
 app.secret_key = b'dde02f8c8fb32d19708474f175f062559a3239e5224f651ff6b79c030cf53d70'
 
 # Initialize database (flask-sqlalchemy) in the app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://workoutapp.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workoutapp.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Added due to warning suggesting it be set to False
 db = SQLAlchemy(app)
+
+# User Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(100), unique=False, nullable=False)
+    lname = db.Column(db.String(100), unique=False, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phoneNo = db.Column(db.String(100), unique=True, nullable=False)
+    bYear = db.Column(db.Integer, unique=False, nullable=False)
+    bMonth = db.Column(db.Integer, unique=False, nullable=False)
+    bDay = db.Column(db.Integer, unique=False, nullable=False)
+    gender = db.Column(db.Integer, unique=False, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+    weight = db.Column(db.Float, unique=False, nullable=False)
 
 # Add authentication (flask-login) to the application
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.get(user_id)
+
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        form = request.form
+        hash = generate_password_hash(form['password'])
+        fields = [key for key in Users.__dict__.keys() if not key.startswith('_') or key == 'id'] 
+
+        # Confirm all required fields were sent
+        for field in fields:
+            if field not in form.keys():
+                return f"{field} is a required field!"
+
+        # Check if user exists, redirect if exists
+        user = Users.query.filter_by(username=form['username'])
+        if user:
+            return 'User already exists.'
+
+        # Create a new user
+        user = Users(
+            fname = form['fname'],       lname = form['lname'],
+            password = form['password'], email = form['email'],
+            phoneNo = form['phoneNo'],   bYear = form['bYear'],
+            bMonth = form['bMonth'],     bDay = form['bDay'],
+            gender = form['gender'],     height = form['height'],
+            weight = form['weight']
+        )
+
+        # Add user to the database
+        db.session.add(user)
+        db.session.commit()
+
+        #redirect to the login page
+        return redirect("/login")
+    
+    return render_template("signup.html")
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login"
+    if request.method == "POST":
+        # Ensure all fields were submitted
+        pass
+        # Check if the user exists
+    return
 
 
 @app.route("/logout", methods=["GET", "POST"])
